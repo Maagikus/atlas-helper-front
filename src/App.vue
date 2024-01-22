d175ced4666e4d98d2cb5ee1bcb0f1e21adc28f6
Horinchoi Yan <maagikus@gmail.com>
2024-01-22 22:52:36 +0200

    add select fsources

--- src/App.vue
+++ src/App.vue
@@ -1,113 +1,158 @@
 <template>
-<div class="container">
-  <div class="wrapper">
-    <form @submit.prevent="onSubmit(form)" id="myForm" class="form">
-      <div class="form__item">
-        <label for="loop">Количество итераций:</label>
-        <input v-model="form.loop" type="number" id="loop" name="loop" required>
-      </div>
+  <div class="container">
+    <div class="wrapper">
+      <form @submit.prevent="onSubmit(form)" id="myForm" class="form">
+        <div class="form__item">
+          <label for="loop">Количество итераций:</label>
+          <input
+            v-model="form.loop"
+            type="number"
+            id="loop"
+            name="loop"
+            required
+          />
+        </div>
 
-      <div class="form__item">
-        <label for="food">Количество еды:</label>
-        <input v-model="form.food" type="number" id="food" name="food" required>
-      </div>
+        <div class="form__item">
+          <label for="food">Количество еды:</label>
+          <input
+            v-model="form.food"
+            type="number"
+            id="food"
+            name="food"
+            required
+          />
+        </div>
 
-      <div class="form__item">
-        <label for="time">Время в минутах:</label>
-        <input v-model="form.time" type="number" id="time" name="time" required>
-      </div>
+        <div class="form__item">
+          <label for="time">Время в минутах:</label>
+          <input
+            v-model="form.time"
+            type="number"
+            id="time"
+            name="time"
+            required
+          />
+        </div>
 
-      <div class="form__item">
-        <label for="resource">Ресурс:</label>
-        <input v-model="form.resource" type="text" id="resource" name="resource" required>
-      </div>
+        <div class="form__item">
+          <label for="resource">Ресурс:</label>
+          <select
+            v-model="form.resource"
+            id="resource"
+            name="resource"
+            required
+          >
+            <option
+              v-for="resource in resources"
+              :key="resource"
+              :value="resource"
+            >
+              {{ resource }}
+            </option>
+          </select>
+        </div>
 
-      <div class="form__item">
-        <label for="planet">planet:</label>
-        <input v-model="form.planet" type="text" id="planet" name="planet" required>
-      </div>
+        <div class="form__item">
+          <label for="planet">planet:</label>
+          <input
+            v-model="form.planet"
+            type="text"
+            id="planet"
+            name="planet"
+            required
+          />
+        </div>
 
-      <div class="form__item">
-        <label for="fleet">fleet:</label>
-        <input v-model="form.fleet" type="text" id="fleet" name="fleet" required>
-      </div>
+        <div class="form__item">
+          <label for="fleet">fleet:</label>
+          <input
+            v-model="form.fleet"
+            type="text"
+            id="fleet"
+            name="fleet"
+            required
+          />
+        </div>
 
-      <div class="form__item">
-        <label for="key">Пароль:</label>
-        <input v-model="form.key" type="text" id="key" name="key" required>
-      </div>
+        <div class="form__item">
+          <label for="key">Пароль:</label>
+          <input v-model="form.key" type="text" id="key" name="key" required />
+        </div>
 
+        <div class="error-message" v-if="errorMessage">{{ errorMessage }}</div>
+        <div class="success-message" v-if="successMessage">
+          {{ successMessage }}
+        </div>
 
-      <div class="error-message" v-if="errorMessage">{{ errorMessage }}</div>
-      <div class="success-message" v-if="successMessage">{{ successMessage }}</div>
-
-
-      <button type="submit">Отправить</button>
-    </form>
-
+        <button type="submit">Отправить</button>
+      </form>
+    </div>
   </div>
-
-</div>
-
 </template>
 
 <script setup>
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import {reactive, ref} from 'vue';
-
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import { reactive, ref } from "vue";
+const resources = ref([
+  "arco",
+  "biomass",
+  "carbon",
+  "diamond",
+  "hydrogen",
+  "iron_ore",
+  "lumanite",
+  "rochinol",
+]);
 const form = reactive({
-  loop: '',
-  food: '',
-  time: '',
-  key: '',
-  resource: '',
-  fleet: '',
+  loop: "",
+  food: "",
+  time: "",
+  key: "",
+  resource: resources.value[0],
+  fleet: "",
 });
 
-const errorMessage = ref('');
-const successMessage = ref('');
-
-
-
+const errorMessage = ref("");
+const successMessage = ref("");
 
 const onSubmit = async (data) => {
   try {
-    successMessage.value = 'Работаем'
-    errorMessage.value = ''
-    const response = await fetch('https://staratlas-helper.onrender.com/start', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
+    successMessage.value = "Работаем";
+    errorMessage.value = "";
+    const response = await fetch(
+      "https://staratlas-helper.onrender.com/start",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
 
     if (!response.ok) {
       // Обрабатывайте ошибки сервера здесь
-      const res = await response.json()
-      successMessage.value = ''
-      errorMessage.value = `Перейди в исходную точку в игре ${res.error}`
+      const res = await response.json();
+      successMessage.value = "";
+      errorMessage.value = `Перейди в исходную точку в игре ${res.error}`;
 
-      console.error('Ошибка сервера:', res.error);
+      console.error("Ошибка сервера:", res.error);
     } else {
       // Обработка успешного ответа
       const responseData = await response.json();
-      successMessage.value = 'Все получилось'
-      console.log('Успешный ответ:', responseData);
-
-
+      successMessage.value = "Все получилось";
+      console.log("Успешный ответ:", responseData);
     }
   } catch (error) {
     // Обрабатывайте сетевые ошибки здесь
-    successMessage.value = ''
-    errorMessage.value = `Перейди в исходную точку в игре ${error}`
+    successMessage.value = "";
+    errorMessage.value = `Перейди в исходную точку в игре ${error}`;
 
-    console.error('Ошибка сети:', error);
-
+    console.error("Ошибка сети:", error);
   }
 };
-
 </script>
 
 <style scoped>
@@ -124,21 +169,21 @@ body {
   color: red;
   margin-top: 10px;
 }
-.success-message{
+.success-message {
   color: green;
   margin-top: 10px;
 }
-.container{
+.container {
   max-width: 1617px;
   margin: 0 auto;
 }
-.wrapper{
-  //display: flex;
-  //align-items: center;
-  //justify-content: center;
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
   padding-left: 300px;
   padding-right: 300px;
-
 }
 @media screen and (max-width: 1200px) {
   .wrapper {
@@ -161,6 +206,7 @@ body {
   }
 }
 form {
+  width: 100%;
   text-align: center;
   padding: 20px;
   border: 1px solid #ccc;
@@ -170,14 +216,13 @@ form {
   flex-direction: column;
   gap: 10px;
 }
-.form__item{
+.form__item {
   display: flex;
   flex-direction: column;
   gap: 5px;
 }
 label {
   display: block;
-
 }
 
 input {
