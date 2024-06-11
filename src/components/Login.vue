44c8469a520b7271e177fcad2e9685e78f27b8d3
Horinchoi Yan <maagikus@gmail.com>
2024-06-11 13:39:02 +0300

    changed link for google auth

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,36 +1,3 @@
-<!-- <template>
-    <form @submit.prevent="onLogin(loginForm)" class="mt-5 col-lg-6 offset-lg-3 d-flex flex-column gap-3">
-        <h2 class="text-center">Sign In</h2>
-        <div class="">
-            <label for="exampleInputEmail1 " class="form-label">Email address</label>
-            <input
-                @blur="v$.email.$touch"
-                v-model="v$.email.$model"
-                type="email"
-                :class="{ 'is-invalid': v$.email.$error }"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-            />
-            <div class="text-danger" v-for="error in v$.email.$errors" :key="error.$uid">
-                {{ error.$message }}
-            </div>
-        </div>
-        <div class="">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input :class="{ 'is-invalid': v$.password.$error }" @blur="v$.password.$touch" v-model="v$.password.$model" type="password" class="form-control" id="exampleInputPassword1" />
-            <div class="text-danger" v-for="error in v$.password.$errors" :key="error.$uid">
-                {{ error.$message }}
-            </div>
-        </div>
-        <span v-if="authStore.errors[0]" class="text-danger">{{ authStore.errors[0] }}</span>
-        <button :disabled="v$.$invalid" type="submit" class="btn btn-primary">Sign In</button>
-        <router-link class="btn btn-primary" to="/registration">Sign Up</router-link>
-      <a href="http://localhost:4040/my-app/v1/auth/google"><i class="bi bi-google"></i>Sign in with Google</a>
-        <a href="http://localhost:4040/my-app/v1/auth/google"><i class="bi bi-google"></i>Sign in with Google</a>
-    </form>
-</template> -->
-
 <template>
     <section class="auth">
         <div class="auth__container">
@@ -82,7 +49,7 @@
                 <div class="auth__footer footer-auth">
                     <div class="footer-auth__header">or continue with</div>
                     <div class="footer-auth__google">
-                        <a href="http://localhost:4040/my-app/v1/auth/google" class="footer-auth__button google"><img src="../assets/images/Google.svg" alt="Google" /> <span>Google</span></a>
+                        <a href="https://api.labs-helper.site/my-app/v1/auth/google" class="footer-auth__button google"><img src="../assets/images/Google.svg" alt="Google" /> <span>Google</span></a>
                     </div>
                     <div class="footer-auth__privacy">By clicking continue, you agree to our <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a></div>
                 </div>
